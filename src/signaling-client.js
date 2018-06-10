4b18e4534edac2642312c598d206f9b4d7346f9a
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-06-10 18:44:56 +0300

    Removed unnecessary log messages from signaling client.

--- src/signaling-client.js
+++ src/signaling-client.js
@@ -29,7 +29,7 @@ exports.SignalingClient = class {
             }
             if (this.id) {
                 const objectJson = JSON.stringify(object)
-                console.log('signaling client sent message ' + objectType + ' : ' + objectJson)
+                //console.log('signaling client sent message ' + objectType + ' : ' + objectJson)
                 self.webSocket.send(JSON.stringify(new Message(this.id, targetId, objectType, objectJson)))
             }
         }
@@ -44,7 +44,7 @@ exports.SignalingClient = class {
             }
             self.state = self.State.CONNECTING
             self.webSocket = new W3CWebSocket(url, 'webrtc-signaling');
-            console.log('signaling client connecting ' + url)
+            //console.log('signaling client connecting ' + url)
         }
 
         this.onConnected = (id) => {
@@ -71,25 +71,25 @@ exports.SignalingClient = class {
 
         this.webSocket.onerror = (error) => {
             if (this.id) {
-                console.log('signaling client connection error');
+                //console.log('signaling client connection error');
                 self.onConnectionError(error)
                 self.disconnect()
             } else {
-                console.log('signaling client connect failed');
+                //console.log('signaling client connect failed');
                 self.state = self.State.CONNECTION_FAILED
                 self.onConnectFailed(error);
             }
         };
 
         this.webSocket.onclose = () => {
-            console.log('signaling client disconnected');
+            //console.log('signaling client disconnected');
             self.state = self.State.DISCONNECTED
             self.onDisconnect();
         };
 
         this.webSocket.onopen = () => {
-            console.log('signaling client connected');
-            console.log('signaling client handshake started')
+            //console.log('signaling client connected');
+            //console.log('signaling client handshake started')
             self.webSocket.send(JSON.stringify(new HandshakeRequest(email, secret)))
         };
 
@@ -98,7 +98,7 @@ exports.SignalingClient = class {
                 const messageObject = JSON.parse(message.data)
                 if (messageObject.typeName === 'HandshakeResponse') {
                     if (messageObject.id) {
-                        console.log('signaling client handshake complete: ' + messageObject.id);
+                        //console.log('signaling client handshake complete: ' + messageObject.id);
                         self.id = messageObject.id
                         self.state = self.State.CONNECTED
                         self.onConnected(messageObject.id);
@@ -109,7 +109,7 @@ exports.SignalingClient = class {
                     }
                 }
                 if (messageObject.typeName === 'Message') {
-                    console.log('signaling client received message : ' + messageObject.contentType + ' : ' + messageObject.contentJson + ' from ' + messageObject.sourceId)
+                    //console.log('signaling client received message : ' + messageObject.contentType + ' : ' + messageObject.contentJson + ' from ' + messageObject.sourceId)
                     self.onReceive(messageObject.sourceId, messageObject.contentType, JSON.parse(messageObject.contentJson));
                 }
             }
